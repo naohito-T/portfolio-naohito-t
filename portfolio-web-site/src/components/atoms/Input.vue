fc5dc1e3efc035efcd1d7bd5570c2212ea9501d9
naohito-T <naohito.tanaka0523@gmail.com>
2021-07-27 00:29:28 +0900

    update feature/login_firebase_setup

---
+++ portfolio-web-site/src/components/atoms/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <lable>
+    <input type="text" v-model="userData.name"/>
+  </lable>
+</template>
+
+<script lang="ts">
+import defineComponent from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props {
+
+  },
+  setup(
+    return {
+    }
+  )
+})
+
+</script>
+
