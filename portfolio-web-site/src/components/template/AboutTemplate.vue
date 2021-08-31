446df80fc72c2b454510e0238660ce5122d85b5d
naohito-T <naohito.tanaka0523@gmail.com>
2021-09-01 00:57:51 +0900

    aboutをpage to templateにした

---
+++ portfolio-web-site/src/components/template/AboutTemplate.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="contents">
+    <section class="section1">
+      <Title :en-title="enTitle" :ja-title="jaTitle" />
+    </section>
+    <section class="section2">
+      <AboutMarkdown />
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import Title from '@/components/common/Title.vue';
+import AboutMarkdown from '@/components/molecules/AboutMarkdown.vue';
+
+export default defineComponent({
+  components: {
+    Title,
+    AboutMarkdown,
+  },
+  props: {
+    enTitle: {
+      type: String,
+      required: true,
+    },
+    jaTitle: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.contents {
+  .section1 {
+    @include inner();
+  }
+
+  .section2 {
+    width: 100%;
+  }
+}
+</style>
--- portfolio-web-site/src/pages/about/index.vue
+++ portfolio-web-site/src/pages/about/index.vue
@@ -1,39 +1,18 @@
 <template>
-  <main class="contents">
-    <section class="section1">
-      <Title :en-title="'About'" :ja-title="'naohito-Tについて'"/>
-    </section>
-    <section class="section2">
-      <AboutMarkdown />
-    </section>
-  </main>
+  <AboutTemplate :enTitle="'ABOUT'" :jaTitle="'naohito-Tについて'" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
-import Title from '@/components/common/Title.vue';
-import AboutMarkdown from '@/components/molecules/AboutMarkdown.vue';
+import AboutTemplate from '@/components/template/AboutTemplate.vue';
 
 
 export default defineComponent({
   components: {
-    Title,
-    AboutMarkdown,
+    AboutTemplate
   },
   setup() {
     return {}
   },
 });
 </script>
-
-<style lang="scss" scoped>
-.contents {
-  .section1 {
-    @include inner();
-  }
-
-  .section2 {
-    width: 100%;
-  }
-}
-</style>
